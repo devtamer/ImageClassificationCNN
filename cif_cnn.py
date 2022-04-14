221bd9b782bb7e2fce769c28d5f70f9d78096c9c
devtamer <whiletam@gmail.com>
2022-04-13 19:41:11 -0700

    READme updated and requirements.txt added

--- cif_cnn.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
-import numpy as np
-import matplotlib.pyplot as plt
-
-(X_train, y_train_), (X_test, y_test_) = datasets.cifar10.load_data()
-X_train.shape
---
+++ readme.md
@@ -0,0 +1,11 @@
+Before running this program, it's recommended you enter a virtual env via
+
+```
+python3 -m venv .venv
+```
+
+next install the necessary packages via
+
+```
+pip install -r requirements.txt
+```
